9e1700c099fd8fde2f416d097e44e360ccca5b30
Erik Bänder <e.baender@ostfalia.de>
2020-06-08 19:43:03 +0200

    bug fixes

--- java-server/src/main/java/extra/Standard.java
+++ java-server/src/main/java/extra/Standard.java
@@ -6,6 +6,6 @@ public class Standard {
     public static final String KEY = "key";
     public static final String STA = "state";
     public static final String COD = "code";
-    public static final String PRG = "session_progress";
+    public static final String PRG = "sessionProgress";
     
 }
